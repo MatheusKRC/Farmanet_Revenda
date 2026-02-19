bdedee7a14f2c7fc3f41c6da66ee6a13678ef7a0
MatheusKRC <kyracraft0@gmail.com>
2026-02-19 17:54:35 -0300

    update config.js

--- src/BackEnd/config/config.js
+++ src/BackEnd/config/config.js
@@ -11,12 +11,12 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
+  host: process.env.HOSTNAME || process.env.DB_HOST || 'localhost',
+  port: process.env.PORT || '3306',
   database: 
-    `${process.env.MYSQL_DB_NAME || 'farmanetd'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'Kyracraft12!',
+    `${process.env.DB_NAME || 'farmanetd'}${suffix[environment] || suffix.test}`,
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'Kyracraft12!',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
