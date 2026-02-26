1cfede0584356095aa7f2ebb3094d06c5ba7bf44
MatheusKRC <kyracraft0@gmail.com>
2026-02-26 09:15:35 -0300

    update routes

--- src/BackEnd/api/Routes/estoqueRoutes.js
+++ src/BackEnd/api/Routes/estoqueRoutes.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
-const estoqueControllers = require('../Controllers/estoqueControllers');
+const estoqueControllers = require('../Controllers/estoqueControllers')
 
 const estoqueRoutes = Router();
 
-estoqueRoutes.get('/estoques', estoqueControllers.getAll);
+estoqueRoutes.get('/estoque', estoqueControllers.getAll);
 estoqueRoutes.post('/estoque', estoqueControllers.getEstoqueByCod);
 estoqueRoutes.post('/estoques', estoqueControllers.postEstoque);
 estoqueRoutes.patch('/estoque', estoqueControllers.patchEstoque)
