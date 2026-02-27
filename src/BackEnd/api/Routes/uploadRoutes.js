431902465029edec07ee93872b1fc9dc0131a441
MatheusKRC <kyracraft0@gmail.com>
2026-02-27 09:10:36 -0300

    upload routes

---
+++ src/BackEnd/api/Routes/uploadRoutes.js
@@ -0,0 +1,106 @@
+const { Router } = require('express');
+const { spawn } = require("child_process");
+const multer = require("multer");
+
+const upload = multer();
+const uploadRoutes = Router();
+
+uploadRoutes.post('/uploadestoque', upload.single("file"), (req, res) => {
+  
+    console.log("bateu na rota uploadestoque");
+    console.log("req.file:", req.file);
+    console.log("req.body:", req.body);
+    const python = spawn(".venv/bin/python", [
+      "Relatorio/RelatorioEstoque.py"
+    ]);
+    const htmlContent = req.file.buffer.toString("utf-8");
+  
+  
+    let result = '';
+    let error = '';
+  
+    python.stdout.on('data', (data) => {
+      result += data.toString();
+    });
+  
+    python.stderr.on('data', (data) => {
+      error += data.toString();
+    });
+  
+    python.on('close', (code) => {
+      if (code !== 0) {
+        console.error('Erro Python:', error);
+        return res.status(500).json({ error });
+      }
+  
+      try {
+        const produtos = JSON.parse(result);
+        res.json({
+          success: true,
+          data: produtos
+        });
+      } catch (err) {
+        res.status(500).json({
+          success: false,
+          data: [],
+        });
+      }
+    });
+  
+    if (!req.file) {
+      return res.status(400).json({ error: "Arquivo não enviado" });
+    }
+    
+    python.stdin.write(htmlContent);
+    python.stdin.end();
+  });
+
+uploadRoutes.post('/uploadsaidas', upload.single("file"), (req, res) => {
+    console.log("req.file:", req.file);
+    console.log("req.body:", req.body);
+    const python = spawn(".venv/bin/python", [
+      "Relatorio/RelatorioSaidas.py"
+    ]);
+    const htmlContent = req.file.buffer.toString("utf-8");
+  
+  
+    let result = '';
+    let error = '';
+  
+    python.stdout.on('data', (data) => {
+      result += data.toString();
+    });
+  
+    python.stderr.on('data', (data) => {
+      error += data.toString();
+    });
+  
+    python.on('close', (code) => {
+      if (code !== 0) {
+        console.error('Erro Python:', error);
+        return res.status(500).json({ error });
+      }
+  
+      try {
+        const produtos = JSON.parse(result);
+        res.json({
+          success: true,
+          data: produtos
+        });
+      } catch (err) {
+        res.status(500).json({
+          success: false,
+          data: [],
+        });
+      }
+    });
+  
+    if (!req.file) {
+      return res.status(400).json({ error: "Arquivo não enviado" });
+    }
+    
+    python.stdin.write(htmlContent);
+    python.stdin.end();
+  });
+
+module.exports = uploadRoutes
--- src/BackEnd/app.js
+++ src/BackEnd/app.js
@@ -1,6 +1,4 @@
 /* eslint-disable no-magic-numbers */
-const multer = require("multer");
-const { spawn } = require("child_process");
 const express = require('express');
 const userRoutes = require('./api/Routes/userRoutes');
 const productsRoutes = require('./api/Routes/productsRoutes');
@@ -8,9 +6,9 @@ const collectorRoutes = require('./api/Routes/collectorsRoutes');
 const cors = require('cors');
 const saidasRoutes = require("./api/Routes/saidasRoutes");
 const estoqueRoutes = require("./api/Routes/estoqueRoutes");
+const uploadRoutes = require("./api/Routes/uploadRoutes");
 
 const app = express();
-const upload = multer();
 
 app.use(cors({
   origin: "*",
@@ -24,102 +22,8 @@ app.use(productsRoutes)
 app.use(collectorRoutes)
 app.use(saidasRoutes)
 app.use(estoqueRoutes)
+app.use(uploadRoutes)
 app.use(express.static('public'));
-app.post('/uploadestoque', upload.single("file"), (req, res) => {
-  console.log("bateu na rota uploadestoque");
-  console.log("req.file:", req.file);
-  console.log("req.body:", req.body);
-  const python = spawn("python3", [
-    "Relatorio/RelatorioEstoque.py"
-  ]);
-  const htmlContent = req.file.buffer.toString("utf-8");
-
-
-  let result = '';
-  let error = '';
-
-  python.stdout.on('data', (data) => {
-    result += data.toString();
-  });
-
-  python.stderr.on('data', (data) => {
-    error += data.toString();
-  });
-
-  python.on('close', (code) => {
-    if (code !== 0) {
-      console.error('Erro Python:', error);
-      return res.status(500).json({ error });
-    }
-
-    try {
-      const produtos = JSON.parse(result);
-      res.json({
-        success: true,
-        data: produtos
-      });
-    } catch (err) {
-      res.status(500).json({
-        success: false,
-        data: [],
-      });
-    }
-  });
-
-  if (!req.file) {
-    return res.status(400).json({ error: "Arquivo não enviado" });
-  }
-  
-  python.stdin.write(htmlContent);
-  python.stdin.end();
-});
-app.post('/uploadsaidas', upload.single("file"), (req, res) => {
-  console.log("req.file:", req.file);
-  console.log("req.body:", req.body);
-  const python = spawn("python3", [
-    "Relatorio/RelatorioSaidas.py"
-  ]);
-  const htmlContent = req.file.buffer.toString("utf-8");
-
-
-  let result = '';
-  let error = '';
-
-  python.stdout.on('data', (data) => {
-    result += data.toString();
-  });
-
-  python.stderr.on('data', (data) => {
-    error += data.toString();
-  });
-
-  python.on('close', (code) => {
-    if (code !== 0) {
-      console.error('Erro Python:', error);
-      return res.status(500).json({ error });
-    }
-
-    try {
-      const produtos = JSON.parse(result);
-      res.json({
-        success: true,
-        data: produtos
-      });
-    } catch (err) {
-      res.status(500).json({
-        success: false,
-        data: [],
-      });
-    }
-  });
-
-  if (!req.file) {
-    return res.status(400).json({ error: "Arquivo não enviado" });
-  }
-  
-  python.stdin.write(htmlContent);
-  python.stdin.end();
-});
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
